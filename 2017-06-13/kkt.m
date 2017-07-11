0d19c0a2f8be881b78a486c46e85094ea6a08a2d
Mattia Setzu <davine.research@gmail.com>
2017-07-11 15:31:59 +0200

    Added exercise 2 for exam on 6-13

---
+++ 2017-06-13/kkt.m
@@ -0,0 +1,44 @@
+clear all; clc;
+syms x1 x2 l1 l2;
+
+foo = x1 + x2;
+g1 = x1^2 + x2^2 - 5;
+g2 = x1^2 - 1;
+lagrangian = foo + l1*g1 + l2*g2;
+gr_lagrangian = gradient(lagrangian, [x1 x2]);
+
+gr_foo = gradient(foo, [x1 x2]);
+gr_g1 = gradient(g1, [x1 x2]);
+gr_g2 = gradient(g2, [x1 x2]);
+
+eq0 = gr_foo(1) + l1*gr_g1(1) + l2*gr_g2(1);
+eq1 = gr_foo(2) + l1*gr_g1(2) + l2*gr_g2(2);
+eq2 = l1*g1;
+eq3 = l2*g2;
+
+res = solve([eq0 eq1 eq2 eq3], [x1 x2 l1 l2]);
+
+for i = 1 : size(res.x1, 1)
+    res_x1 = res.x1(i);
+    res_x2 = res.x2(i);
+    res_l1 = res.l1(i);
+    res_l2 = res.l2(i);
+    
+    if (res_l1 >= 0 && res_l2 >= 0 && subs(g1, [x1 x2], [res_x1 res_x2]) <= 0 && subs(g2, [x1 x2], [res_x1 res_x2]) <= 0)
+        fprintf('Minimum: %f\t %f\t %f\t %f\n', res_x1, res_x2, res_l1, res_l2);
+    end
+end
+
+min_x_lagrangian = solve([gr_lagrangian(1) gr_lagrangian(2)], [x1 x2]);
+
+fprintf('Lagrangian');
+lagrangian
+fprintf('\nLagrangian gradient');
+gr_lagrangian
+fprintf('\nMin x Lagrangian');
+min_x_lagrangian.x1
+min_x_lagrangian.x2
+fprintf('\nRelaxation');
+lagrangian_relaxation = subs(lagrangian, [x1 x2], [min_x_lagrangian.x1 min_x_lagrangian.x2])
+fprintf('\nRelaxation in (1,0)');
+subs(lagrangian_relaxation, [l1 l2], [1 0])
